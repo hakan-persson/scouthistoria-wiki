6975b30fc272b61efc4e04ef9be9f6cb4c4b3ae9
hakan-persson <hakan@mabadiliko.se>
2024-01-08 22:38:48 +0100

    Now correct setup.sh

--- rootfs/opt/scouterna/scouthistoria-wiki/config/setup.sh
+++ rootfs/opt/scouterna/scouthistoria-wiki/config/setup.sh
@@ -13,38 +13,18 @@ set -o nounset
 # Load library with logging functions
 . /opt/bitnami/scripts/liblog.sh
 
-## Update LocalSettings.php file with some settings
-cat /opt/scouterna/scouthistoria-wiki/config/misc-settings.txt >> "$MEDIAWIKI_CONF_FILE"
+# Update LocalSettings.php file with our settings
+cat /opt/scouterna/scouthistoria-wiki/config/local-settings.txt >> "$MEDIAWIKI_CONF_FILE"
 
-## Update DB with Semantic stuff
+# Copy logo to the right place
+cp /opt/scouterna/scouthistoria-wiki/images/sams.png $MEDIAWIKI_BASE_DIR/images/sams.png
+
+# Update DB with Semantic stuff
 # cd /opt/bitnami/mediawiki/extensions/SemanticMediaWiki/maintenance
 # MW_INSTALL_PATH="/opt/bitnami/mediawiki" php setupStore.php
 
-# Configure logo
-
-## Update LocalSettings.php file
-cat <<EOF >> "$MEDIAWIKI_CONF_FILE"
-# Logo settings
-# \$wgLogo = "/skins/Vector/resources/src/logo.png";
-# \$wgFavicon = "/skins/Vector/resources/src/favicon.ico";
-# \$wgFooterIcons['poweredby']['scoutwiki'] = array(
-#     'src' => "/skins/Vector/resources/src/poweredby_scoutwiki.png",
-#     'url' => "https://scoutwiki.scouterna.se/",
-#     'alt' => "ScoutWiki",
-# );
-\$wgLogos = [
-        '1x' => "$wgResourceBasePath/images/sams.png",
-        'icon' => "$wgResourceBasePath/images/sams.png",
-];
-EOF
-
-## Copy logo to the right place
-cp /opt/scouterna/scouthistoria-wiki/images/sams.png $MEDIAWIKI_BASE_DIR/images/sams.png
-
-# Configure extensions
-
-# Update LocalSettings.php file
-cat /opt/scouterna/scouthistoria-wiki/config/extension-settings.txt >> "$MEDIAWIKI_CONF_FILE"
-
-# Create an init bot
-/opt/scouterna/scouthistoria-wiki/scripts/create_bot.sh
+# Create a init bot user
+cd /opt/bitnami/mediawiki
+BOT_PWD=$(cat /dev/urandom | tr -dc '[:digit:]' | fold -w ${1:-32} | head -n 1)
+BOT_INFO=$(php maintenance/createBotPassword.php ${MEDIAWIKI_USERNAME^} $BOT_PWD --appid initbot --grants basic,highvolume,createeditmovepage,delete,uploadeditmovefile,editsiteconfig)
+info $BOT_INFO
